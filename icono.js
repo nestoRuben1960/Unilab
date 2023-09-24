51fe5df03cafe66a9d02fee8b7179b604f9e9e87
nestoRuben1960 <129061156+nestoRuben1960@users.noreply.github.com>
2023-09-24 18:11:27 -0300

    Add files via upload

---
+++ icono.js
@@ -0,0 +1,16 @@
+var visible = false; 
+            function iniciar() { 
+            var elemento = document.getElementById("menu-img"); 
+            elemento.addEventListener("click", mostrarMenu); 
+            } 
+            function mostrarMenu() { 
+            var elemento = document.getElementById("menu"); 
+            if (!visible) { 
+            elemento.style.display = "flex"; 
+            visible = true; 
+            } else { 
+            elemento.style.display = "none"; 
+            visible = false; 
+            } 
+            } 
+            window.addEventListener("load", iniciar);
